860e860f7e35198772f7f1534e1c470c44ce6a9a
Hely-T~ <dvfb.hmtg@gmail.com>
2024-05-11 12:56:18 +0700

    Update Surge5

--- Tool/Surge5/Auto_join_TF.js
+++ Tool/Surge5/Auto_join_TF.js
@@ -2,10 +2,10 @@
 Thời gian cập nhật: 2024.05.11 10:40
 Nội dung được cập nhật: Đã thêm tính năng lưu giữ hoặc biến mất chậm theo danh mục thông báo, âm thanh nhắc đóng mô-đun (tham số SurgeTF)
 
-Suger5
-https://raw.githubusercontent.com/Hely-T/TestFlight-All/master/Tool/Suger5/Surge/AUTOTF.sgmodule
+Surge5
+https://raw.githubusercontent.com/Hely-T/TestFlight-All/master/Tool/Surge5/Surge/AUTOTF.sgmodule
 BoxJS
-https://raw.githubusercontent.com/Hely-T/TestFlight-All/master/Tool/Suger5/boxjs.json
+https://raw.githubusercontent.com/Hely-T/TestFlight-All/master/Tool/Surge5/boxjs.json
 */
 
 if (typeof $request !== 'undefined' && $request) {
